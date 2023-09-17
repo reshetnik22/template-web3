200451abd305e2a1a2e39d76a70814b4f4703ea5
reshetnik22 <116947679+reshetnik22@users.noreply.github.com>
2023-09-17 15:14:58 +0200

    Update env.mjs

--- env.mjs
+++ env.mjs
@@ -3,3 +3,6 @@ import { z } from 'zod'
 
 export const env = createEnv({
   server: {
+    // Iron session requires a secret of at least 32 characters
+    NEXTAUTH_SECRET: z.string().min(32).default('complex_password_at_least_32_characters_long'),
+    DATABASE_URL: z.string().url().optional(),
